645069b0afd943cd65b89178e9f03b2a37638a51
Melissa Nunes <mnunes@sparkgeo.com>
2023-03-03 13:03:13 -0700

    WIP

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import { useState } from "react";
-import Map from "react-map-gl";
+import { useState } from 'react'
+import Map, { Layer, Source } from 'react-map-gl'
+import { DenverTrafficFatalities } from './dataForDemo/DenverTrafficFatalities'
+import './App.css'
 
-import "./App.css";
+const layerStyle = {
+  id: 'point',
+  type: 'circle',
+  paint: {
+    'circle-radius': 10,
+    'circle-color': '#007cbf',
+  },
+}
 
 function App() {
   const [view, setViewport] = useState({
     longitude: -104.9847,
     latitude: 39.73915,
     zoom: 10,
-  });
+  })
 
   return (
     <Map
       initialViewState={{ ...view }}
-      style={{ width: "100%", height: "100vh" }}
+      style={{ width: '100%', height: '100vh' }}
       mapStyle="mapbox://styles/mapbox/streets-v9"
       onViewportChange={(viewport) => setViewport(viewport)}
-    />
-  );
+    >
+      <Source id="my-data" type="geojson" data={DenverTrafficFatalities}>
+        <Layer {...layerStyle} />
+      </Source>
+    </Map>
+  )
 }
 
 // document.addEventListener("DOMContentLoaded", () => map.resize());
 
-export default App;
+export default App
